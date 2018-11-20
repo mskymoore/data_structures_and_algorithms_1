3ccd26f9c2324ee30191286170187540f0cb7c3d
Sky Moore <mskymoore@gmail.com>
2018-11-19 21:44:08 -0800

    Added algorithms expecting duplicates

--- ch2/2_7_4_and_5.py
+++ ch2/2_7_4_and_5.py
@@ -1,4 +1,4 @@
-import random, sys, copy, time, timeit, matplotlib, os, threading
+import random, timeit, matplotlib, threading
 import matplotlib.pyplot as plt
 
 
@@ -12,17 +12,42 @@ def find_smallest(a_list):
     return smallest_i
 
 
-def find_kth_smallest_O_n(a_list, k):
-    count = 0
-    while count < (k - 1):
+def find_kth_smallest_O_n_no_duplicates(a_list, k):
+    for i in range(0, k - 1):
         a_list.pop(find_smallest(a_list))
-        count += 1
     return a_list.pop(find_smallest(a_list))
 
 
-def find_kth_smallest_O_nlogn(a_list, k):
+def find_kth_smallest_O_nlogn_no_duplicates(a_list, k):
     a_list.sort()
     return a_list[k - 1]
+
+
+def find_kth_smallest_O_n(a_list, k):
+    if k == 1:
+        return a_list.pop(find_smallest(a_list))
+    seen = dict()
+    count = 0
+    while count <= k - 1:
+        num = a_list.pop(find_smallest(a_list))
+        if num not in seen:
+            seen[num] = None
+            count += 1
+    return num
+
+
+def find_kth_smallest_O_nlogn(a_list, k):
+    a_list.sort(reverse=True)
+    if k == 1:
+        return a_list.pop()
+    seen = dict()
+    count = 0
+    while count <= k - 1:
+        num = a_list.pop()
+        if num not in seen:
+            seen[num] = None
+            count += 1
+    return num
 
 
 
@@ -38,14 +63,14 @@ class time_algorithm(threading.Thread):
     def run(self):
         for i, size in enumerate(self.values, start=1):
             self.progress = i
-            self.a_list = [ random.randint(0,1000) for i in range(size) ]
+            self.a_list = [ random.randint(0,1000000) for i in range(size) ]
             self.times.append(self.timer.timeit(number=10))
-        
+
 
 
 k = 3
 
-x_axis = list(range(1000, 1000000, 10000))
+x_axis = list(range(1000, 1000000, 5000))
 
 O_n_timer = timeit.Timer('find_kth_smallest_O_n(threads[0].a_list, k)',
                          'from __main__ import threads, k, find_kth_smallest_O_n')
@@ -58,25 +83,21 @@ threads = [time_algorithm(x_axis, O_n_timer, 'O(n)'),
 
 num_threads = len(threads)
 len_x_axis = len(x_axis)
-len_str_len_x_axis = len(str(len_x_axis)
 for i, t in enumerate(threads, start=1):
-    print(f'Starting thread {i} of {num_threads}: {t.name}\n')
+    print(f'Starting algorithm {i} of {num_threads}: {t.name}\n\n')
     t.start()
     while True:
-        print(u'\u001b[A\u001b[1000D', end='')
-        
+        print(u'\u001b[2A\u001b[1000D', end='')
         if t.progress == len_x_axis:
-            print(f'Thread {i} complete      ', ' '*len(str(t.progress)),
-                   '    ', ' '*len_str_len_x_axis, '[', '='*101, ']\n',
-                   sep='', end='')
+            last_symbol='='
         else:
-            bars = int((t.progress / len_x_axis)*100) - 1
-            spaces = 100 - bars
-            print(f'Thread {i} running test {t.progress} of {len(x_axis)} [',
-                   '='*bars, '>', ' '*spaces, ']\n', sep='', end='')
-        
-      
-        t.join(timeout=0.5)
+            last_symbol='>'
+        bars = int((t.progress / len_x_axis)*100)
+        spaces = 100 - bars
+        print(f'Running test {t.progress} of {len(x_axis)}\n[',
+               '='*bars, last_symbol, ' '*spaces, ']\n', sep='', end='')
+    
+        t.join(timeout=0.2)
         
         if not t.is_alive():
             break
@@ -88,6 +109,6 @@ ax.set_ylabel('execution time (seconds)')
 ax.set_title('kth smallest item in a list')
 ax.plot(x_axis, threads[0].times, 'r')
 ax.plot(x_axis, threads[1].times, 'b')
-ax.legend([threads[0].name, threads[1].name])
+ax.legend([a.name for a in threads])
 # fig.savefig('kth_smallest_fig.png')
 plt.show()
